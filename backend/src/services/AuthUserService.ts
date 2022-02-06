2d0c72efe52c7853999a03fc45074456efa39e31
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-02-06 15:18:41 -0300

    create auth user service

---
+++ backend/src/services/AuthUserService.ts
@@ -0,0 +1,58 @@
+import prismaClient from "../prisma";
+import bcrypt from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { NotFound, Unauthorized } from "http-errors"
+
+type UserData = {
+  name?: string
+  email: string
+  password: string
+}
+
+class AuthUserService {
+  async register(data: UserData) {
+
+    data.password = bcrypt.hashSync(data.password, 8);
+
+    const user = prismaClient.user.create({
+      data
+    })
+
+    return user;
+  }
+
+  async login(data: UserData) {
+    const { email, password } = data;
+
+    const user = await prismaClient.user.findUnique({
+      where: { email }
+    })
+
+    if (!user) throw new NotFound("User not registered")
+
+    const checkPassword = bcrypt.compareSync(password, user.password);
+
+    if (!checkPassword) throw new Unauthorized("Email address or password not valid")
+
+    delete user.password
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          email: user.email,
+          id: user.id
+        }
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d"
+      }
+    )
+
+    return { ...user, token }
+  }
+}
+
+export default AuthUserService;
