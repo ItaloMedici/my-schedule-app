18f70f54213ca76ff5cdcad10206f855166b678c
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-02-06 16:51:46 -0300

    create controller's exportation file

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,5 @@
+import AuthUserController from "./AuthUserController";
+import ClientController from "./ClientController";
+import ScheduleController from "./ScheduleController";
+
+export { AuthUserController, ClientController, ScheduleController }
