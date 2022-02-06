6082a2c30b3b96a1a6cb1976375d10067d796dbc
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-02-06 18:33:50 -0300

    add crud on client service/controller

--- backend/src/controllers/ClientController.ts
+++ backend/src/controllers/ClientController.ts
@@ -4,10 +4,46 @@ import ClientService from "../services/ClientService";
 class ClientController {
   static async create(req: Request, res: Response) {
     const client = req.body;
+    try {
+      const result = await ClientService.create(client);
+      
+      return res.json(result)
+    } catch (error) {
+      return res.json({error: error.message})
+    }
 
-    const result = await ClientService.create(client);
+  }
 
-    return res.json(result)
+  static async getAll(req: Request, res: Response) {
+    const result = await ClientService.findAll();
+
+    return res.json(result);
+  }
+
+  static async get(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const result = await ClientService.find(id)
+
+      return res.json(result);
+
+    } catch (err) {
+      res.json({ error: err.message })
+    }
+  }
+
+  static async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    
+    try {
+      const result = await ClientService.delete(id)
+
+      return res.json(result);
+
+    } catch (err) {
+      res.json({ error: err.message })
+    }
   }
 }
 
--- backend/src/services/ClientService.ts
+++ backend/src/services/ClientService.ts
@@ -1,15 +1,52 @@
 import { Client } from "../models";
 import prismaClient from "../prisma";
+import { NotFound, Conflict } from "http-errors"
+import { Prisma } from "@prisma/client";
 
 class ClientService {
   static async create(clientInfo: Client) {
-    const client = await prismaClient.client.create({
-      data: {
-        ...clientInfo
+
+    try {
+      const client = await prismaClient.client.create({
+        data: {
+          ...clientInfo
+        }
+      });
+  
+      return client
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          return new Conflict("There is a unique constraint violation, check that has to be unique")
+        }
       }
+    }
+
+  }
+
+  static async findAll() {
+    const allClients = await prismaClient.client.findMany();
+    return allClients;
+  }
+
+  static async find(id: string) {
+    const client = await prismaClient.client.findUnique({
+      where: { id }
     });
 
-    return client
+    if (!client) return new NotFound("Client not found");
+
+    return client;
+  }
+
+  static async delete(id: string) {
+    const client = await prismaClient.client.delete({
+      where: { id }
+    });
+
+    if (!client) return new NotFound("Client not found");
+
+    return client;
   }
 }
 
