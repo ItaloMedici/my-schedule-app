a18051941c55f0fcce6e932f3a2540e08e35ef5d
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-02-13 03:47:19 -0300

    init project

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+
+import GlobalStyle from './styles/GlobalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 )
