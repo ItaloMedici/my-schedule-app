de1cf7b0e9fd11901859edc0ad15d26042e12c36
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-02-13 03:50:18 -0300

    Home page

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+import { OutlinedInput, TextField } from '@mui/material';
+import React from 'react';
+import Button from '../../components/Button';
+import SearchField from '../../components/Fields/SearchField';
+import Navbar from '../../components/Navbar';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      {/* <Button label='Novo Atendimento' onClick={(e) => {}} /> */}
+      <Navbar />
+    </>
+  );
+}
+
+export default Home;
