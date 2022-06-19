a6691408fbafbc230983a4fa5b6828f176adc0b5
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-06-19 04:14:50 -0300

    add: global style

--- web/src/styles/GlobalStyle.ts
+++ web/src/styles/GlobalStyle.ts
@@ -1,41 +1,19 @@
-import { createGlobalStyle } from "styled-components";
+import { globalCss } from "../theme/stitches.config";
 
-export default createGlobalStyle`
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+export default globalCss({
+  '*': {
+    margin: '0',
+    padding: '0',
+    boxSizing: 'border-box',
+  },
 
-    html {
-    min-height: 100%;
-    background: var(--bg);
-    overflow: hidden;
+  html: {
+    minHeight: '100%',
+    background: '$backgrond',
+    overflow: 'hidden'
+  },
 
-    --horizontalPadding: 224px;
-
-  }
-
-  @media (max-width: 1112px) {
-    * {
-      --horizontalPadding: 160px;
-    }
-  }
-
-  @media (max-width: 952px) {
-    * {
-      --horizontalPadding: 100px;
-    }
-  }
-
-  @media (max-width: 830px) {
-    * {
-      --horizontalPadding: 50px;
-    }
-  }
-
-
-  @media (max-width: 1080px){
+/*   @media (max-width: 1080px){
     html {
       font-size: 93.75%; // 15px
     }
@@ -45,29 +23,13 @@ export default createGlobalStyle`
     html{
       font-size: 87.5%; // 14px 
     }
-  }
+  } */
 
-  button{
-    cursor: pointer;  
-  }
 
-  *, button, input {
-    border: 0;
-    background: none;
-    font-family: "Roboto", -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
-    color: var(--black);
+  '*, button, input': {
+    border: '0',
+    background: 'none',
+    fontFamily: '$main',
+    color: '$textColor',
   }
-
-  :root {
-    --white: #FFF;
-    --bg: #F5F5F5;
-    --gray-50: #F7F8FA;
-    --gray-100: #E6E8EB;
-    --gray-200: #AFB2B1;
-    --gray-500: #808080;
-    --gray-800: #494D4B;
-
-    --primary: #DFA290;
-    --primary-light: #EDB09F;
-  }
-`;
+})
---
+++ web/src/styles/MainContainer.ts
@@ -0,0 +1,11 @@
+import { styled } from "../theme/stitches.config";
+
+
+export const MainContainer = styled('div', {
+  horizontalPadding: '',
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
