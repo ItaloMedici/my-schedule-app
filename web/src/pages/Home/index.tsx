6a58267dc70e3703b3b5add2734f9637d42bcbc9
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-06-19 04:16:59 -0300

    Removing MUI dependecy

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
-import { Divider } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import Button from '../../components/Button';
+import { Button } from '../../components/Button';
 import ButtonIcon from '../../components/ButtonIcon';
 import SearchField from '../../components/Fields/SearchField';
 import Navbar from '../../components/Navbar';
-import AddIcon from '@mui/icons-material/Add';
-
+import { PlusIcon } from '@radix-ui/react-icons'
 
 
 import { Main, Header, Wrapper } from './styles';
+import Schedule from '../../components/Schedule';
 
 const Home: React.FC = () => {
   return (
@@ -19,24 +18,24 @@ const Home: React.FC = () => {
           <h1>Atendimentos</h1>
           <Wrapper>
             <SearchField />
-            <Button 
-              label='Novo Atendimento' 
+            <Button
               onClick={(e) => { }}
-              startIcon = {
-                <AddIcon />
-              }
-            />
+            >
+              Novo Atendimento
+              <PlusIcon />
+            </Button>
             <ButtonIcon
               onClick={() => { }}
-              bg={"var(--primary)"}
+            //bg={"var(--primary)"}
             >
-              <AddIcon />
+              <PlusIcon />
             </ButtonIcon>
           </Wrapper>
         </Header>
         <SearchField />
-        <Divider />
-
+        <hr />
+        <Schedule />
+        <Schedule />
       </Main>
     </>
   );
--- web/src/pages/Home/styles.ts
+++ web/src/pages/Home/styles.ts
@@ -1,66 +1,64 @@
-import styled from 'styled-components';
+import { styled } from "../../theme/stitches.config";
 
-export const Container = styled.div`
-  
-`;
+export const Container = styled('div', {});
 
-export const Main  = styled.div`
-  margin: 0 var(--horizontalPadding);
+export const Main = styled('div', {
+  margin: '0 var(--horizontalPadding)',
 
-  display: flex;
-  flex-direction: column;
+  display: 'flex',
+  flexDirection: 'column',
 
-  > :nth-child(2) {
-    display: none
-  }
+  '> :nth-child(2)': {
+    display: 'none'
+  },
 
-  @media (max-width: 500px) {
-    > :nth-child(2) {
-      display: inline-flex;
-      margin-bottom: 10px;
+  '@media (max-width: 500px) ': {
+    '> :nth-child(2) ': {
+      display: 'inline-flex',
+      marginBottom: '10px',
     }
   }
-  
-`;
+});
 
-export const Header  = styled.div`
-  display: flex;
-  align-items: center;
+export const Header = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
 
-  padding: 40px 0 10px;
-  
-  justify-content: flex-end;
-  gap: 18px;
+  padding: '40px 0 10px',
+
+  justifyContent: 'flex-end',
+  gap: '18px',
 
 
-  h1 {
-    flex: 1;
+  'h1': {
+    flex: '1',
   }
-`;
+});
 
-export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
 
-  gap: 18px;
+export const Wrapper = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
 
-  > :nth-child(3) {
-    display: none
-  }
+  gap: '18px',
 
-  @media (max-width: 700px) {  
-    > :nth-child(2) {
-      display: none
+  '> :nth-child(3)': {
+    display: 'none'
+  },
+
+  '@media (max-width: 700px)': {
+    '> :nth-child(2)': {
+      display: 'none'
+    },
+
+    '> :nth-child(3)': {
+      display: 'inline-flex',
     }
+  },
 
-    > :nth-child(3) {
-      display: inline-flex;
+  '@media (max-width: 500px)': {
+    '> :nth-child(1)': {
+      display: 'none'
     }
   }
-
-  @media (max-width: 500px) {
-    > :nth-child(1) {
-      display: none
-    }
-  }
-`;
+});
