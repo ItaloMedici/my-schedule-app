eb0c33904d7b8369fad85c951b6e5f0ef6fbe3d8
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-03 01:52:26 -0300

    fix main container layout

--- web/src/styles/MainContainer.ts
+++ web/src/styles/MainContainer.ts
@@ -2,10 +2,19 @@ import { styled } from "../theme/stitches.config";
 
 
 export const MainContainer = styled('div', {
-  horizontalPadding: '',
   height: '100vh',
   display: 'flex',
+  flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
+
+  horizontalPadding: '',
+
+  variants: {
+    bg: {
+      primary: {background: '$backgrond'},
+      secondary: {background: '$gray2'},
+    }
+  }
 });
 
