49702740af7710ebcbd10a9ab1b5eb833c50e200
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-03 01:53:15 -0300

    fix: auth layout

--- web/src/pages/Auth/index.tsx
+++ web/src/pages/Auth/index.tsx
@@ -1,19 +1,24 @@
 import { EnterIcon } from '@radix-ui/react-icons';
 import React, { useEffect, useState } from 'react';
+import { useIntl } from 'react-intl';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Button';
 
-import ButtonIcon from '../../components/ButtonIcon';
 import InputPassword from '../../components/Fields/InputPassword';
 import { InputText } from '../../components/Fields/InputText';
+import { CardForm } from '../../components/Form/CardForm';
 import { User } from "../../models/User";
+import { authenticate } from '../../services/AuthService';
 import { MainContainer } from '../../styles/MainContainer';
-import { Card } from './styles';
 
 interface State extends User {
   showPassword: boolean;
 }
 
 const Auth: React.FC = () => {
+  const { formatMessage } = useIntl();
+  const navigate = useNavigate();
+
   const [values, setValues] = useState<State>({
     email: "",
     password: "",
@@ -25,43 +30,58 @@ const Auth: React.FC = () => {
   const handleChange = (e) => {
     setValues({
       ...values,
-      [e.target.name]: [e.target.value]
+      [e.target.name]: e.target.value
     });
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { email, password } = values;
+
+    await authenticate({ email, password })
+      .then(resp => {
+        console.log("ok:", resp)
+        navigate('/home', { replace: true })
+      })
+      .catch(err => {
+        console.log("erroo:", err)
+      })
   }
 
   return (
     <MainContainer>
-      <Card>
-        <div>
-          <h1>Login</h1>
-        </div>
+      <CardForm>
+        <h1>{formatMessage({ id: "label.login" })}</h1>
 
-        <form>
+        <form onSubmit={handleLogin}>
           <InputText
-            label='Email' 
-            onChange={handleChange} 
-            value={values.email} 
-            name="email" 
+            idLabel='label.email'
+            onChange={handleChange}
+            value={values.email}
+            name="email"
+            size='md'
           />
 
           <InputPassword
-            label='Senha' 
-            onChange={handleChange} 
-            value={values.password} 
-            name="password" 
+            idLabel='label.password'
+            onChange={handleChange}
+            value={values.password}
+            name="password"
+            size='md'
           />
 
-
           <Button
-          //onClick={() => { }}
+            onClick={handleLogin}
+            idLabel='label.login'
+            size='md'
           >
-            Sign In
             <EnterIcon color='#FFF' />
           </Button>
         </form>
 
-        <a href='/register'>Don't have an account? <span>Sign up</span></a>
-      </Card>
+        <Link to='/register'>{formatMessage({ id: "messages.signIn" })}</Link>
+      </CardForm>
     </MainContainer>
   );
 }
