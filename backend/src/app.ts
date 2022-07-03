ddee97006b9b8d81ecd7b80253a94ff107ac155a
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-03 01:49:49 -0300

    add: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,13 @@
 import "dotenv/config"
 import express from "express";
 import { router } from "./routes";
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
 
 app.listen(4000, () => console.log(`🚀 App rodando na porta 4000`))
