374187fed4f9761738ab2f43fb53e1a04bf036fb
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-03 21:25:26 -0300

    add: feed model

---
+++ web/src/models/Feed.ts
@@ -0,0 +1,21 @@
+export interface ScheduleRes {
+  appointment: string;
+  id: string;
+  description: string;
+  price: number;
+  finished: boolean;
+  client: {
+      id: string;
+      name: string;
+  };
+}
+
+export interface ScheduleWeek {
+  label?: string;
+  schedules: ScheduleRes[];
+}
+
+export interface ScheduleFeed {
+  avaliable: ScheduleWeek[];
+  overdue: ScheduleWeek;
+}
