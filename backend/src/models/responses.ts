986f9750875976c49d7d4067bfc62f496d309b24
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-03 21:23:58 -0300

    response model

---
+++ backend/src/models/responses.ts
@@ -0,0 +1,23 @@
+import { Decimal } from "@prisma/client/runtime";
+
+export class ScheduleRes {
+  appointment: Date;
+  id: string;
+  description: string;
+  price: Decimal;
+  finished: boolean;
+  client: {
+      id: string;
+      name: string;
+  };
+}
+
+export class ScheduleWeek {
+  label?: Date;
+  schedules: ScheduleRes[] = [];
+}
+
+export class ScheduleFeed {
+  avaliable: ScheduleWeek[] = []
+  overdue: ScheduleWeek = new ScheduleWeek();
+}
