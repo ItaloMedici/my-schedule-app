deac586b969a18513739c483eb7ab056659b7193
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-24 18:26:31 -0300

    implementing cruf

--- web/src/pages/ScheduleForm/index.tsx
+++ web/src/pages/ScheduleForm/index.tsx
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import Checkbox from '../../components/Checkbox';
+import DialogDelete from '../../components/DialogDelete';
 import { InputText } from '../../components/Fields/InputText';
 import InputTextArea from '../../components/Fields/InputTextArea';
 import { ToolBarForm } from '../../components/ToolBarForm';
 import { useAuth } from '../../contexts/auth';
 import { useToast } from '../../contexts/toast';
 import { Schedule } from '../../models/Schedule';
-import { getSchedule } from '../../services/ScheduleService';
+import { deleteSchedule, getSchedule, saveOrUpdateSchedule } from '../../services/ScheduleService';
 
 import { Container, DateWrapper, StatusWrapper } from './styles';
 
 const ScheduleForm: React.FC = () => {
   const { user } = useAuth();
   const { formatDate, formatTime, formatNumber } = useIntl();
-  const { showError } = useToast();
+  const { showError, showSuccess } = useToast();
+  const navigate = useNavigate();
   const { idSchedule } = useParams();
   const [schedule, setSchudule] = useState<Schedule>();
   const [allowEdit, setAllowEdit] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
 
   useEffect(() => {
     if (idSchedule) {
@@ -38,68 +41,93 @@ const ScheduleForm: React.FC = () => {
 
   }, [])
 
+  const handleSave = async () => {
+    if (schedule) {
+      const response = await saveOrUpdateSchedule(schedule);
+      if (response.status === 200) {
+        showSuccess('Registrado salvo com sucesso!');
+        navigate('/', { replace: true })
+      }
+    }
+  }
 
+  const handleDelete = async () => {
+    if (schedule?.id) {
+      const response = await deleteSchedule(schedule.id);
+
+      if (response.status === 200) {
+        showSuccess('Registrado deletado com sucesso!');
+        navigate('/', {replace: true})
+      }
+    }
+  }
 
   return (
-    <Container>
-      <ToolBarForm
-        allowEdit={allowEdit}
-        onEdit={() => setAllowEdit(true)}
-      />
-      {schedule &&
-        <>
-          <StatusWrapper>
+    <>
+      <Container>
+        <ToolBarForm
+          allowDelete={!!schedule?.id}
+          allowEdit={allowEdit}
+          onEdit={() => setAllowEdit(true)}
+          onSave={handleSave}
+          onDelete={() => setShowDialog(true)}
+        />
+        {schedule &&
+          <>
+            <StatusWrapper>
+              <InputText
+                name="description"
+                value={schedule.description}
+                idLabel="label.description"
+                disabled={!allowEdit}
+                css={{ flex: 1 }}
+              />
+
+            </StatusWrapper>
+            <DateWrapper>
+              <InputText
+                name="appointment"
+                value={formatDate(schedule.appointment)}
+                idLabel="label.date"
+                disabled={!allowEdit}
+              />
+              <InputText
+                name="appointment"
+                value={formatTime(schedule.appointment)}
+                idLabel="label.time"
+                disabled={!allowEdit}
+              />
+            </DateWrapper>
             <InputText
-              name="description"
-              value={schedule.description}
-              idLabel="label.description"
+              name="price"
+              value={formatNumber(schedule.price || 0, { style: 'currency', currency: 'BRL' })}
+              idLabel="label.price"
               disabled={!allowEdit}
-              css={{ flex: 1 }}
+            />
+            <InputText
+              name="client"
+              value={schedule.client}
+              idLabel="label.customer"
+              disabled={!allowEdit}
+            />
+            <Checkbox
+              idLabel='label.finishedAppointment'
+              checked={schedule.finished}
+              onCheck={(checked) => setSchudule({ ...schedule, finished: checked })}
+              disabled={!allowEdit}
+            />
+            <InputTextArea
+              name="observation"
+              value={schedule.observation}
+              idLabel="label.observation"
+              disabled={!allowEdit}
             />
 
-          </StatusWrapper>
-          <DateWrapper>
-            <InputText
-              name="appointment"
-              value={formatDate(schedule.appointment)}
-              idLabel="label.date"
-              disabled={!allowEdit}
-            />
-            <InputText
-              name="appointment"
-              value={formatTime(schedule.appointment)}
-              idLabel="label.time"
-              disabled={!allowEdit}
-            />
-          </DateWrapper>
-          <InputText
-            name="price"
-            value={formatNumber(schedule.price || 0, { style: 'currency', currency: 'BRL' })}
-            idLabel="label.price"
-            disabled={!allowEdit}
-          />
-          <InputText
-            name="client"
-            value={schedule.client}
-            idLabel="label.customer"
-            disabled={!allowEdit}
-          />
-          <Checkbox
-            idLabel='label.finishedAppointment'
-            checked={schedule.finished}
-            onCheck={(checked) => setSchudule({ ...schedule, finished: checked })}
-            disabled={!allowEdit}
-          />
-          <InputTextArea
-            name="observation"
-            value={schedule.observation}
-            idLabel="label.observation"
-            disabled={!allowEdit}
-          />
-        </>
-      }
-
-    </Container>
+          </>
+        }
+      </Container>
+      <DialogDelete open={showDialog} onHide={() => setShowDialog(false)} onDelete={handleDelete} />
+    </>
   );
 }
 
