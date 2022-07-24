c5d60cdd9c2ed35c1600eb2fc82dffff15e975c6
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-24 13:55:06 -0300

    create base for inputs

---
+++ web/src/components/Fields/Base/index.ts
@@ -0,0 +1,69 @@
+import { css, styled } from "../../../theme/stitches.config";
+
+export const ContainerBase = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+
+  position: 'relative',
+})
+
+export const LabelBase = css({
+  fontSize: '$sm',
+  marginBottom: '$2'
+});
+
+export const FieldBase = css({
+    width: '100%',
+    backgroundColor: '$background',
+    borderRadius: '$md',
+    border: '1px solid $gray5',
+  
+    fontSize: '$md',
+  
+    marginBottom: '$4',
+  
+    '&:focus': {
+      outline: 'none',
+      borderColor: '$primary',
+      boxShadow: '$primary'
+    },
+  
+    transition: 'background-color 0.2s, color 0.2s, border-color 0.2s, box-shadow 0.2s',
+  
+    '&:disabled': {
+      opacity: .7
+    },
+  
+    variants: {
+      size: {
+        xs: {
+          padding: '$1'
+        },
+        sm: {
+          padding: '$2'
+        },
+        md: {
+          padding: '$3'
+        },
+        xl: {
+          padding: '$4'
+        }
+      },
+  
+      invalid: {
+        true: {
+          borderColor: '$error',
+          
+          '&:focus': {
+            boxShadow: '$error',
+            borderColor: '$error',
+          }
+        }
+      },
+    },
+  
+    defaultVariants: {
+      size: 'sm'
+    }
+})
