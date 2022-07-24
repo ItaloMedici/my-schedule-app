d572468754ad4f1e24315632a304c24ee2946845
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-24 02:27:58 -0300

    removing fetching feed

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -3,28 +3,22 @@ import React, { useEffect, useState } from 'react';
 import { Button } from '../../components/Button';
 import SearchField from '../../components/Fields/SearchField';
 import Navbar from '../../components/Navbar';
-
 import { useIntl } from 'react-intl';
-import Schedule from '../../components/Schedule';
+import Feed from '../../components/Feed';
 import { useToast } from '../../contexts/toast';
 import { ScheduleFeed } from '../../models/Feed';
 import { buildFeed } from '../../services/ScheduleService';
 import { Header, Main, Wrapper } from './styles';
-import Feed from '../../components/Feed';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
   const { formatMessage, formatDate } = useIntl();
-  const {showWarn, showError} = useToast();
-  const [feed, setFeed] = useState<ScheduleFeed>();
+  const { showWarn, showError } = useToast();
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    buildFeed()
-      .then(resp => setFeed(resp.data))
-      .catch(err => {
-        let {messageCode} = err?.response?.data;
-        showError(formatMessage({id: `errors.${messageCode}`}))
-      })
-  }, []);
+  const handleNewAppointment = () => {
+    navigate('/atendimento');
+  }
 
   return (
     <>
@@ -32,24 +26,24 @@ const Home: React.FC = () => {
 
       <Main>
         <Header>
-          <h1>{formatMessage({id: "label.appointments"})}</h1>
+          <h1>{formatMessage({ id: "label.appointments" })}</h1>
           <Wrapper>
             <SearchField />
             <Button
-              onClick={(e) =>  showWarn("Erro ao abrir", "Conferir erro")}
+              onClick={handleNewAppointment}
               idLabel="label.newAppointment"
             >
               <PlusIcon />
             </Button>
             <Button
-              onClick={() => { }}
+              onClick={handleNewAppointment}
             >
               <PlusIcon />
             </Button>
           </Wrapper>
         </Header>
         <SearchField />
-        <Feed feed={feed} />
+        <Feed />
       </Main>
 
     </>
