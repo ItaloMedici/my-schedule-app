a44d4c4397cc195ed19ea7a3f198790fd2f918b6
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-24 16:37:46 -0300

    add: checkbox input

--- web/src/pages/ScheduleForm/index.tsx
+++ web/src/pages/ScheduleForm/index.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useLocation, useParams } from 'react-router-dom';
+import Checkbox from '../../components/Checkbox';
 import { InputText } from '../../components/Fields/InputText';
+import InputTextArea from '../../components/Fields/InputTextArea';
 import { ToolBarForm } from '../../components/ToolBarForm';
+import { useAuth } from '../../contexts/auth';
 import { useToast } from '../../contexts/toast';
 import { Schedule } from '../../models/Schedule';
 import { getSchedule } from '../../services/ScheduleService';
 
-import { Container, DateWrapper } from './styles';
+import { Container, DateWrapper, StatusWrapper } from './styles';
 
 const ScheduleForm: React.FC = () => {
+  const { user } = useAuth();
   const { formatDate, formatTime, formatNumber } = useIntl();
   const { showError } = useToast();
   const { idSchedule } = useParams();
-  const [schedule, setSchudule] = useState<Schedule>(new Schedule());
+  const [schedule, setSchudule] = useState<Schedule>();
   const [allowEdit, setAllowEdit] = useState(false);
 
   useEffect(() => {
@@ -26,7 +30,9 @@ const ScheduleForm: React.FC = () => {
           showError(error.message)
         }
       })()
-    } else {
+    } else if (!schedule) {
+      console.log("S")
+      setSchudule(new Schedule(user))
       setAllowEdit(true)
     }
 
@@ -40,38 +46,59 @@ const ScheduleForm: React.FC = () => {
         allowEdit={allowEdit}
         onEdit={() => setAllowEdit(true)}
       />
-      <InputText
-        name="description"
-        value={schedule.description}
-        idLabel="label.description"
-        disabled={!allowEdit}
-      />
-      <DateWrapper>
-        <InputText
-          name="appointment"
-          value={formatDate(schedule.appointment)}
-          idLabel="label.date"
-          disabled={!allowEdit}
-        />
-        <InputText
-          name="appointment"
-          value={formatTime(schedule.appointment)}
-          idLabel="label.time"
-          disabled={!allowEdit}
-        />
-      </DateWrapper>
-      <InputText
-        name="price"
-        value={formatNumber(schedule.price || 0, {style: 'currency', currency: 'BRL'})}
-        idLabel="label.price"
-        disabled={!allowEdit}
-      />
-      <InputText
-        name="client"
-        value={schedule.client}
-        idLabel="label.customer"
-        disabled={!allowEdit}
-      />
+      {schedule &&
+        <>
+          <StatusWrapper>
+            <InputText
+              name="description"
+              value={schedule.description}
+              idLabel="label.description"
+              disabled={!allowEdit}
+              css={{ flex: 1 }}
+            />
+
+          </StatusWrapper>
+          <DateWrapper>
+            <InputText
+              name="appointment"
+              value={formatDate(schedule.appointment)}
+              idLabel="label.date"
+              disabled={!allowEdit}
+            />
+            <InputText
+              name="appointment"
+              value={formatTime(schedule.appointment)}
+              idLabel="label.time"
+              disabled={!allowEdit}
+            />
+          </DateWrapper>
+          <InputText
+            name="price"
+            value={formatNumber(schedule.price || 0, { style: 'currency', currency: 'BRL' })}
+            idLabel="label.price"
+            disabled={!allowEdit}
+          />
+          <InputText
+            name="client"
+            value={schedule.client}
+            idLabel="label.customer"
+            disabled={!allowEdit}
+          />
+          <Checkbox
+            idLabel='label.finishedAppointment'
+            checked={schedule.finished}
+            onCheck={(checked) => setSchudule({ ...schedule, finished: checked })}
+            disabled={!allowEdit}
+          />
+          <InputTextArea
+            name="observation"
+            value={schedule.observation}
+            idLabel="label.observation"
+            disabled={!allowEdit}
+          />
+        </>
+      }
+
     </Container>
   );
 }
--- web/src/pages/ScheduleForm/styles.ts
+++ web/src/pages/ScheduleForm/styles.ts
@@ -1,13 +1,24 @@
-import { styled } from "../../theme/stitches.config";
+import { InputText } from "../../components/Fields/InputText";
+import { css, styled } from "../../theme/stitches.config";
 
 export const Container = styled('div', {
   horizontalPadding: '',
   
 })
 
-export const DateWrapper = styled('div', {
+const inline = css({
   display: 'flex',
   gap: '$4',
+})
+
+export const StatusWrapper = styled('div', {
+  ...inline,
+  alignItems: 'center',
+})
+
+export const DateWrapper = styled('div', {
+  ...inline,
+
   
 
 })
