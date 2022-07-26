772064e5da85138e1ede67ed1731e7fa4966288f
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-26 05:42:58 -0300

    [✨ NEW] Adding Customer Modal in Selection (#5)

--- web/src/components/CustomerSelect/index.tsx
+++ web/src/components/CustomerSelect/index.tsx
@@ -1,11 +1,13 @@
-import { PlusIcon } from '@radix-ui/react-icons';
-import React, { useEffect, useState } from 'react';
-import { Customer } from '../../models/Client';
-import { getCustomers } from '../../services/CustumerService';
-import { buildFeed } from '../../services/ScheduleService';
-import { Button } from '../Button';
+import { Pencil1Icon, PlusIcon } from '@radix-ui/react-icons';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Customer } from '../../models/Customer';
+import { getCustomers } from '../../services/CustomerService';
 import Select from '../Select';
-import { AddButton, Container } from './styles';
+import { Container, Label, Wrapper } from './styles';
+
+import { useIntl } from 'react-intl';
+import { Button } from '../Button';
+import CustomerModal from '../CustomerModal';
 
 type CustomerSelectType = {
   value?: string;
@@ -14,18 +16,22 @@ type CustomerSelectType = {
   disabled?: boolean
 }
 
-const CustomerSelect: React.FC<CustomerSelectType> = ({value, onChange, name, disabled, ...rest}) => {
+const CustomerSelect: React.FC<CustomerSelectType> = ({ value, onChange, name, disabled, ...rest }) => {
+  const { formatMessage } = useIntl();
   const [customers, setCustomers] = useState<Customer[]>([]);
+  const [openForm, setOpenForm] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      const response = await getCustomers();
-
-      if (response.data) {
-        setCustomers(response.data)
-      }
-    })()
+    (async () => await findCustomers())()
   }, []);
+
+  const findCustomers = async () => {
+    const response = await getCustomers();
+
+    if (response.data) {
+      setCustomers(response.data)
+    }
+  }
 
   const buildCostumerItems = () => {
     return customers.map(c => {
@@ -36,23 +42,52 @@ const CustomerSelect: React.FC<CustomerSelectType> = ({value, onChange, name, di
     })
   }
 
+  const getSelectedCustomer = useCallback(() => {
+    return customers.find(c => c.id === value)
+  }, [customers, value])
+
+  const updateCustomer = useCallback((customerId) => {
+    (async () => await findCustomers())()
+    onChange(customerId);
+  }, [value])
+
   return (
-    <Container>
-      <Select
-        name={name}
-        value={value}
-        onChange={onChange}
-        idLabel='label.custumer'
-        idPlaceholder='label.selectCustumer'
-        items={buildCostumerItems()}
-        disabled={disabled}
-        {...rest}
+    <>
+      <Container>
+        <Label htmlFor='select'>{formatMessage({ id: 'label.customer' })}</Label>
+        <Wrapper>
+          <Select
+            id='select'
+            name={name}
+            value={value}
+            onChange={onChange}
+            idPlaceholder='label.selectCustumer'
+            items={buildCostumerItems()}
+            disabled={disabled}
+            {...rest}
+          />
+          <Button
+            color='neutral'
+            onClick={() => setOpenForm(true)}
+            disabled={disabled}
+          >
+            {value
+              ? <Pencil1Icon />
+              : <PlusIcon />
+            }
+          </Button>
+        </Wrapper>
+      </Container>
+      <CustomerModal
+        open={openForm}
+        onOpenChange={() => setOpenForm(false)}
+        customerId={value}
+        updateCustomer={updateCustomer}
+        getSelectedCustomer={getSelectedCustomer}
       />
-      <AddButton>
-        <PlusIcon />
-      </AddButton>
-    </Container>
+    </>
   );
 }
 
 export { CustomerSelect };
+
