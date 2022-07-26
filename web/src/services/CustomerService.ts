34c99d51283bf5eb310c743a303dbbb4c6d14dce
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-26 05:45:45 -0300

    [✨ NEW] Calling new Customer route (#6)

--- web/src/services/CustomerService.ts
+++ web/src/services/CustomerService.ts
@@ -1,5 +1,10 @@
+import { Customer } from "../models/Customer"
 import { api } from "./api"
 
 export const getCustomers = () => {
   return api.get('/customers')
+}
+
+export const saveCustomer = (customer: Customer) => {
+  return api.post('/customer', customer)
 }
