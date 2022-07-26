6bc8d1ad1c6b8f9e8c0214bb05dac6b5486fb9ff
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-26 05:45:10 -0300

    [🛠️] Removing userId since is already filled (#6)

--- web/src/models/Customer.ts
+++ web/src/models/Customer.ts
@@ -7,13 +7,10 @@ export class Customer {
   phone: string;
   userId: string
 
-  constructor(user: User | null, name: string = "", email: string = "", phone: string = "") {
+  constructor(name: string = "", email: string = "", phone: string = "") {
     this.name = name;
     this.email = email;
     this.phone = phone;
-    if (user) {
-      this.userId = user.id;
-    }
   }
 }
 
